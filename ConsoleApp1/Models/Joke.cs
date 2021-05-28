f35819010decf82a8addcf05011adac3be2d76fc
Andre Machado <andre.machado@nymity.com>
2021-05-28 04:41:30 -0400

    add models

---
+++ ConsoleApp1/Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokeGenerator.Models
+{
+    public class Joke
+    {
+        public string Value { get; set; }
+
+        public void ReplaceNameTo(Name name)
+        {
+            if (string.IsNullOrEmpty(name?.FirstName) || string.IsNullOrEmpty(name.LastName))
+            {
+                return;
+            }
+
+            this.Value = this.Value.Replace("Chuck Norris", $"{name.FirstName} {name.LastName}");
+        }
+    }
+}
---
+++ ConsoleApp1/Models/Name.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace JokeGenerator.Models
+{
+    public class Name
+    {
+        [JsonProperty("name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("surname")]
+        public string LastName { get; set; }
+    }
+}
