ff13810aa88d21fef34a6afc47bbcd6c505a3829
Andre Machado <andre.machado@nymity.com>
2021-05-28 04:40:46 -0400

    code refactoring of Console features

---
+++ ConsoleApp1/Helpers/ConsoleHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JokeGenerator.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void DisplayOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("Press c to get categories");
+            Console.WriteLine("Press r to get random jokes");
+            Console.WriteLine("Press q to quit");
+        }
+
+        public static void WaitForAnyKey()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void PrintResults(bool shouldClearConsole, string header, IEnumerable<string> results)
+        {
+            if (shouldClearConsole)
+            {
+                Console.Clear();
+            }
+
+            Console.WriteLine(header);
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine(string.Join("\n", results));
+            Console.WriteLine();
+        }
+    }
+}
